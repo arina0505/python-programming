82a91af75d07705c59d3958ea0d6f59d4b3a7538
arina0505 <37370156+arina0505@users.noreply.github.com>
2018-04-28 10:33:14 +0530

    Update max among 10 no's.py

--- beginner level/max among 10 no's.py
+++ beginner level/max among 10 no's.py
@@ -1,7 +1,7 @@
 a=[]
 b=int(input('enter the number'))
-for i in range(n):
-     b=int(input('enter the numbers'))
-     a.append(b)
+for i in range(b):
+     c=int(input('enter the numbers'))
+     a.append(c)
 print(a)
 print(max(a))
